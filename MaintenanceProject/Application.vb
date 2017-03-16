af274188fe10cc02a50e6bd32cdcc13a60cb07b3
AEGriffith <AmandaEGriffith@gmail.com>
2017-03-15 19:13:26 -0500

    Finished Bug Fixes

--- MaintenanceProject/Application.vb
+++ MaintenanceProject/Application.vb
@@ -1,11 +1,15 @@
 ﻿Imports System.Data.OleDb
 Public Class Application
 
-    Dim StuFName As String
-    Dim StuLName As String
+    Dim StuName = ApplicantProfile.getName()
     Dim CommentID As Integer
     Dim Comment As String
     Dim Recommendation As String
+    'Get Program Recommendation Options
+    Dim MISDecision = ApplicantProfile.getMISDecision()
+    Dim PMISDecision = ApplicantProfile.getPMISDecision()
+    'Get Program
+    Dim Program = ApplicantProfile.getProgram
 
     Dim ConnInfo As New DBConnectionInfo
         Dim ConnString As String = ConnInfo.GetConnString()
@@ -18,21 +22,19 @@ Public Class Application
         'Check to see if user has already left a recommendation comment - will return a value for Application_Comment_ID
         Dim commandCheckUser As New OleDbCommand("SELECT Application_Comment_ID FROM Application_Comment WHERE User_ID = '" & Username & "' AND Application_ID = '" & AppID & "'", con)
 
-        Dim commandGetFName As New OleDbCommand("SELECT First_Name FROM Application WHERE Application_Id = '" & AppID & "'", con)
-        Dim commandGetLName As New OleDbCommand("SELECT Last_Name FROM Application WHERE Application_Id = '" & AppID & "'", con)
+
         con.ConnectionString = ConnString
         con.Open()
 
         Try
-            StuFName = commandGetFName.ExecuteScalar
-            StuLName = commandGetLName.ExecuteScalar
+
             'executes the check to see if user has alredy left a comment for this application
             CommentID = commandCheckUser.ExecuteScalar
         Catch ex As Exception
             MessageBox.Show(ex.ToString)
         End Try
         con.Close()
-        lblStuName.Text = StuLName & ", " & StuFName
+        lblStuName.Text = StuName
 
         If CommentID = 0 Then
             'Changes Button text - If it is a new comment button text will be "Add", if there is already a comment, button text will be "update"
@@ -58,11 +60,25 @@ Public Class Application
             'Sets the textbox to the text in the comment column in the application_comment table: Will be editable
             tbComment.Text = Comment
             'Sets the track bar to the value in the score column in the application_comment table: Will be editable
-            ComboBox_suggesteddecision.SelectedText = Recommendation
+            cbRecommendation.SelectedText = Recommendation
         End If
+
+        'Populates recommendation combobox based on Program
+        Select Case Program
+            Case "MIS"
+                cbRecommendation.Items.Clear()
+                cbRecommendation.Items.AddRange(MISDecision)
+            Case "PMIS"
+                MessageBox.Show("PMIS")
+                cbRecommendation.Items.Clear()
+                cbRecommendation.Items.AddRange(PMISDecision)
+        End Select
+
     End Sub
+
+    'Stored procedure to update records
     Private Sub UpdateRecord()
-        If ComboBox_suggesteddecision.SelectedIndex > -1 Then
+        If cbRecommendation.Text <> "" Then
             con.ConnectionString = ConnString
 
             Dim sqlComm As New OleDbCommand
@@ -74,12 +90,14 @@ Public Class Application
 
             sqlComm.Parameters.AddWithValue("comment_ID", Integer.Parse(CommentID))
             sqlComm.Parameters.AddWithValue("comment", tbComment.Text)
-            sqlComm.Parameters.AddWithValue("recommend", ComboBox_suggesteddecision.Text)
+            sqlComm.Parameters.AddWithValue("recommend", cbRecommendation.Text)
 
             con.Open()
 
             sqlComm.ExecuteNonQuery()
             con.Close()
+            MessageBox.Show("Date Successfully Updated!")
+            Me.Close()
         Else
             MessageBox.Show("Recommendation required")
         End If
@@ -87,7 +105,7 @@ Public Class Application
     Private Sub InsertNewRecord()
 
 
-        If ComboBox_suggesteddecision.SelectedIndex > -1 Then
+        If cbRecommendation.Text <> "" Then
             con.ConnectionString = ConnString
 
             Dim sqlComm As New OleDbCommand
@@ -100,12 +118,14 @@ Public Class Application
             sqlComm.Parameters.AddWithValue("AppID", Integer.Parse(AppID))
             sqlComm.Parameters.AddWithValue("userID", Integer.Parse(Username))
             sqlComm.Parameters.AddWithValue("comment", tbComment.Text)
-            sqlComm.Parameters.AddWithValue("recommendations", ComboBox_suggesteddecision.Text)
+            sqlComm.Parameters.AddWithValue("recommendations", cbRecommendation.Text)
 
             con.Open()
 
             sqlComm.ExecuteNonQuery()
             con.Close()
+            MessageBox.Show("Data Successfully Added!")
+            Me.Close()
         Else
             MessageBox.Show("Recommendation required")
         End If
@@ -124,8 +144,8 @@ Public Class Application
                 Try
                     'New comment calls InsertNewRecord()
                     InsertNewRecord()
-                    MessageBox.Show("Data Successfully Added!")
-                    Me.Close()
+
+
                 Catch ex As Exception
                     MessageBox.Show("Data was not added.")
                     'MessageBox.Show(ex.ToString) (system message)
@@ -134,8 +154,8 @@ Public Class Application
                 Try
                     'Updating comment calls UpdateRecord()
                     UpdateRecord()
-                    MessageBox.Show("Date Successfully Updated!")
-                    Me.Close()
+
+
                 Catch ex As Exception
                     MessageBox.Show("Data was not updated.")
                 End Try
