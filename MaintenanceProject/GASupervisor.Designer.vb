a09d93992021ff8ab627774f6fd49a167c46d1d7
AEGriffith <AmandaEGriffith@gmail.com>
2017-04-25 14:53:46 -0500

    Finished 4.2.C

--- MaintenanceProject/GASupervisor.Designer.vb
+++ MaintenanceProject/GASupervisor.Designer.vb
@@ -27,7 +27,15 @@ Partial Class GASupervisor
         Me.EditToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.RequestGraduateAssistantToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rb2 = New System.Windows.Forms.RadioButton()
+        Me.rb1 = New System.Windows.Forms.RadioButton()
+        Me.btnClose = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -36,7 +44,7 @@ Partial Class GASupervisor
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem1, Me.ViewToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(838, 49)
+        Me.MenuStrip1.Size = New System.Drawing.Size(943, 49)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -65,17 +73,90 @@ Partial Class GASupervisor
         Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(94, 45)
         Me.ViewToolStripMenuItem.Text = "View"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 84)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(195, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Assigned GAs"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(21, 137)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowTemplate.Height = 40
+        Me.DataGridView1.Size = New System.Drawing.Size(641, 272)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rb2)
+        Me.GroupBox1.Controls.Add(Me.rb1)
+        Me.GroupBox1.Location = New System.Drawing.Point(668, 137)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(263, 160)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Semester"
+        '
+        'rb2
+        '
+        Me.rb2.AutoSize = True
+        Me.rb2.Location = New System.Drawing.Point(25, 96)
+        Me.rb2.Name = "rb2"
+        Me.rb2.Size = New System.Drawing.Size(206, 36)
+        Me.rb2.TabIndex = 1
+        Me.rb2.Text = "Spring 2018"
+        Me.rb2.UseVisualStyleBackColor = True
+        '
+        'rb1
+        '
+        Me.rb1.AutoSize = True
+        Me.rb1.Checked = True
+        Me.rb1.Location = New System.Drawing.Point(25, 54)
+        Me.rb1.Name = "rb1"
+        Me.rb1.Size = New System.Drawing.Size(170, 36)
+        Me.rb1.TabIndex = 0
+        Me.rb1.TabStop = True
+        Me.rb1.Text = "Fall 2017"
+        Me.rb1.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(772, 335)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(159, 54)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
         'GASupervisor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(838, 683)
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(943, 439)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "GASupervisor"
         Me.Text = "GASupervisor"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -86,4 +167,10 @@ Partial Class GASupervisor
     Friend WithEvents EditToolStripMenuItem1 As ToolStripMenuItem
     Friend WithEvents RequestGraduateAssistantToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rb2 As RadioButton
+    Friend WithEvents rb1 As RadioButton
+    Friend WithEvents btnClose As Button
 End Class
--- MaintenanceProject/GASupervisor.vb
+++ MaintenanceProject/GASupervisor.vb
@@ -1,5 +1,71 @@
-﻿Public Class GASupervisor
+﻿Imports System.Data.OleDb
+Public Class GASupervisor
+
+    'Setup Connection Variables
+    Dim ConnInfo As New DBConnectionInfo
+    Dim ConnString As String = ConnInfo.GetConnString()
+    Dim con As New OleDbConnection()
+
+    Dim Supervisor_ID = Login.getUsername()
+    Dim Semester = "Fa2017"
+
     Private Sub RequestGraduateAssistantToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RequestGraduateAssistantToolStripMenuItem.Click
         GARequest.Show()
+    End Sub
+
+    Private Sub GetAssignedStudentsHours()
+        con.ConnectionString = ConnString
+
+        Dim sqlComm As New OleDbCommand
+        Dim ds As New DataSet
+
+
+        sqlComm.Connection = con
+
+        'setup for stored procedure
+        sqlComm.CommandText = "procGetAssignedStudentsHours"
+        sqlComm.CommandType = CommandType.StoredProcedure
+        sqlComm.Connection = con
+
+
+        'sets values for stored procedure
+        sqlComm.Parameters.AddWithValue("UserID", Supervisor_ID)
+        sqlComm.Parameters.AddWithValue("Semester", Semester)
+
+
+        con.Open()
+        Dim Adpt = New OleDbDataAdapter(sqlComm)
+        Adpt.Fill(ds)
+        DataGridView1.DataSource = ds.Tables(0)
+
+        con.Close()
+    End Sub
+
+
+    Private Sub GASupervisor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SemesterSelect()
+        GetAssignedStudentsHours()
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+        SemesterSelect()
+        GetAssignedStudentsHours()
+    End Sub
+
+    Private Sub SemesterSelect()
+        If rb1.Checked = True Then
+            Semester = "Fa2017"
+        Else
+            Semester = "Sp2018"
+        End If
+    End Sub
+
+    Private Sub rb1_CheckedChanged(sender As Object, e As EventArgs) Handles rb1.CheckedChanged, rb2.CheckedChanged
+        SemesterSelect()
+        GetAssignedStudentsHours()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
     End Sub
 End Class
